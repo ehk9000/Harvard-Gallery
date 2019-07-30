32f2704be7ea5df109a36eb009e0c3b64c1d4eb1
Erik Konner <erikkonner@gmail.com>
2019-07-30 14:41:10 -0600

    Created .env file and imported for fetch

--- src/utils/fetchPhotos.js
+++ src/utils/fetchPhotos.js
@@ -1,3 +1,5 @@
-export const fetchPhotos = () => {
-  
+export const fetchPhotos = async () => {
+  const url = `https://api.harvardartmuseums.org/object?apikey=${process.env.HARVARD_MUSEUM_API_KEY}&classification=Photographs&hasimage=1`
+  const results = 
+
 };
