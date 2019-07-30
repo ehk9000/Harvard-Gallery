249592dba554b6197b8c430ce16773e0396f1b1a
Erik Konner <erikkonner@gmail.com>
2019-07-30 15:15:45 -0600

    Wrote fetchPhotos function

--- src/utils/fetchPhotos.js
+++ src/utils/fetchPhotos.js
@@ -1,5 +1,22 @@
-export const fetchPhotos = async () => {
-  const url = `https://api.harvardartmuseums.org/object?apikey=${process.env.HARVARD_MUSEUM_API_KEY}&classification=Photographs&hasimage=1`
-  const results = 
+const photoChecker = (results) => {
+  return results.records[0].images.length
+}
 
+const photosCleaner = (photos) => {
+
+};
+
+export const fetchPhotos = async (pageNum) => {
+  const url = `https://api.harvardartmuseums.org/object?apikey=${process.env.HARVARD_MUSEUM_API_KEY}&classification=Photographs&hasimage=1&page=${pageNum}`
+  try {
+    const results =  await fetch (url);
+    if (!photoChecker(results)) {
+      fetchPhotos(pageNum + 1)
+    } else {
+      photosCleaner(results)
+    }
+  }
+  catch(error) {
+    console.log(error)
+  }
 };
