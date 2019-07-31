c19695767257e736b60aa6019bc358af7b1b95e0
Erik Konner <erikkonner@gmail.com>
2019-07-31 09:48:25 -0600

    Wrote express app, added env var for port

--- server.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-
-app = express();
-
---
+++ src/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+
+app.set('port', process.env.PORT || 3000);
+
+
+app.listen(app.get('port'), () => {
+  console.log(`Server running ⚡️ on http://localhost:${app.get('port')}`);
+});
