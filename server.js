f6b5119666587cd583eb5b9c37e604d9dc3825a3
Erik Konner <erikkonner@gmail.com>
2019-07-31 11:14:10 -0600

    Added start script

--- server.js
+++ server.js
@@ -8,4 +8,4 @@ app.use(serveStatic(__dirname + "/dist"));
 const port = process.env.PORT || 5000;
 app.listen(port);
 
-console.log('Server running ⚡️ on' + port);
+console.log('Server running ⚡️ on port ' + port);
