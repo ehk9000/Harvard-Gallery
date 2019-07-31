933bde96645553d1602e067ac22d05a7ff7778d2
Erik Konner <erikkonner@gmail.com>
2019-07-31 12:17:09 -0600

    Env variable troubleshooting

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
+require('dotenv').config()
+const app = express();
 const path = require('path');
 const serveStatic = require('serve-static');
 
-app = express();
 app.use(serveStatic(__dirname + "/dist"));
 
 const port = process.env.PORT || 5000;
+
 app.listen(port);
 
 console.log('Server running ⚡️ on port ' + port);
--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -6,6 +6,7 @@
 
 <script>
 import Photo from './Photo.vue';
+import  { apiKey }  from '../utils/apikey.js';
 
 export default {
   name:'Gallery',
@@ -19,7 +20,7 @@ export default {
       }
     },
     mounted: function() {
-      const url = `https://api.harvardartmuseums.org/image?apikey=${process.env.HARVARD_MUSEUM_API_KEY}&size=100`;
+      const url = `https://api.harvardartmuseums.org/image?apikey=${apiKey}&size=100`;
         fetch(url)
           .then(response => response.json())
           .then(results => (this.images = results.records))
